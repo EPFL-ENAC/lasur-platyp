ccfd459e43ac6d8f67363c1f22a5c829f28b6d2a
ymarcon <yannick.marcon@epfl.ch>
2025-03-27 18:18:04 +0100

    fix: declare module for md files

--- collect/src/utils/benefits.ts
+++ collect/src/utils/benefits.ts
@@ -1,34 +1,18 @@
-// @ts-expect-error works with qmarkdown
 import VeloEnMd from 'src/assets/benefits/velo-en.md'
-// @ts-expect-error works with qmarkdown
 import VeloFrMd from 'src/assets/benefits/velo-fr.md'
-// @ts-expect-error works with qmarkdown
 import CovoitEnMd from 'src/assets/benefits/covoit-en.md'
-// @ts-expect-error works with qmarkdown
 import CovoitFrMd from 'src/assets/benefits/covoit-fr.md'
-// @ts-expect-error works with qmarkdown
 import ElecEnMd from 'src/assets/benefits/elec-en.md'
-// @ts-expect-error works with qmarkdown
 import ElecFrMd from 'src/assets/benefits/elec-fr.md'
-// @ts-expect-error works with qmarkdown
 import InterEnMd from 'src/assets/benefits/inter-en.md'
-// @ts-expect-error works with qmarkdown
 import InterFrMd from 'src/assets/benefits/inter-fr.md'
-// @ts-expect-error works with qmarkdown
 import MarcheEnMd from 'src/assets/benefits/marche-en.md'
-// @ts-expect-error works with qmarkdown
 import MarcheFrMd from 'src/assets/benefits/marche-fr.md'
-// @ts-expect-error works with qmarkdown
 import TpuEnMd from 'src/assets/benefits/tpu-en.md'
-// @ts-expect-error works with qmarkdown
 import TpuFrMd from 'src/assets/benefits/tpu-fr.md'
-// @ts-expect-error works with qmarkdown
 import TrainEnMd from 'src/assets/benefits/train-en.md'
-// @ts-expect-error works with qmarkdown
 import TrainFrMd from 'src/assets/benefits/train-fr.md'
-// @ts-expect-error works with qmarkdown
 import VaeEnMd from 'src/assets/benefits/vae-en.md'
-// @ts-expect-error works with qmarkdown
 import VaeFrMd from 'src/assets/benefits/vae-fr.md'
 
 export const benefits: { [key: string]: { en: string; fr: string } } = {
