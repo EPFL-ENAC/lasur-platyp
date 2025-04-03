53b4e68d5163c0abdb2ba7a4591d87946f06aa3b
ymarcon <yannick.marcon@epfl.ch>
2025-04-03 11:49:59 +0200

    feat: terms and privacy step updated

--- collect/src/components/form/SurveyPanel.vue
+++ collect/src/components/form/SurveyPanel.vue
@@ -3,7 +3,7 @@
     <!--pre>{{ survey.step }} - {{ survey.stepName }}</pre-->
     <div v-if="survey.stepName === 'agreement'">
       <SectionItem :label="t('form.agreement')" class="q-mb-lg" />
-      <div class="q-mt-lg">
+      <div class="bg-primary rounded-borders q-pa-md q-mt-lg">
         <q-checkbox
           v-model="survey.record.data.terms_conditions"
           :label="t('form.terms_conditions')"
@@ -12,12 +12,13 @@
           class="text-h6"
         />
         <div class="text-h6 q-ml-xl">
-          <a href="https://modus-ge.ch/toolkit-cgu" target="_blank" class="q-ml-sm">{{
-            t('form.terms_conditions_link')
-          }}</a>
+          <a href="https://modus-ge.ch/toolkit-cgu" target="_blank" class="text-secondary q-ml-sm">
+            {{ t('form.terms_conditions_link') }}
+            <q-icon name="open_in_new" size="xs" />
+          </a>
         </div>
       </div>
-      <div class="q-mt-lg">
+      <div class="bg-primary rounded-borders q-pa-md q-mt-lg">
         <q-checkbox
           v-model="survey.record.data.confidentiality"
           :label="t('form.confidentiality')"
@@ -26,9 +27,14 @@
           class="text-h6"
         />
         <div class="text-h6 q-ml-xl">
-          <a href="https://modus-ge.ch/toolkit-privacy-notice" target="_blank" class="q-ml-sm">{{
-            t('form.confidentiality_link')
-          }}</a>
+          <a
+            href="https://modus-ge.ch/toolkit-privacy-notice"
+            target="_blank"
+            class="text-secondary q-ml-sm"
+          >
+            {{ t('form.confidentiality_link') }}
+            <q-icon name="open_in_new" size="xs"
+          /></a>
         </div>
       </div>
     </div>
