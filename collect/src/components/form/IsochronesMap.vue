4b813d12c2775ebcffe7e49569e95f9363068a24
Hugo Solleder <hsolleder@users.noreply.github.com>
2026-08-10 13:35:54 +0200

    polish: update isochrone color legend (#324)
    Closes #324

--- collect/src/components/form/IsochronesMap.vue
+++ collect/src/components/form/IsochronesMap.vue
@@ -52,7 +52,7 @@
             class="row items-center"
           >
             <div
-              :style="`width: 15px; height: 15px; background-color: ${getCutoffColor(cutoff)}; border: 1px solid #5a3fc0; margin-right: 5px;`"
+              :style="`width: 15px; height: 15px; background-color: rgba(${hexToRgb(getCutoffColor(cutoff))}, 0.3); border: 1px solid #5a3fc0; margin-right: 5px;`"
             ></div>
             <div>{{ t('isochrones.minutes', { count: Math.floor(cutoff / 60) }) }}</div>
           </div>
@@ -418,6 +418,13 @@ function categoryToColor(str: string): { name: string; hex: string } | undefined
 
 function getCutoffColor(cutoff: number): string {
   return ISOCHRONE_CUTOFF_COLORS[cutoff] || '#5a3fc0'
+}
+
+function hexToRgb(hex: string): string {
+  const r = parseInt(hex.slice(1, 3), 16)
+  const g = parseInt(hex.slice(3, 5), 16)
+  const b = parseInt(hex.slice(5, 7), 16)
+  return `${r}, ${g}, ${b}`
 }
 </script>
 
