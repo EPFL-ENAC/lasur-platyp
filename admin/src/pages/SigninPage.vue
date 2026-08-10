dd8bac150fe34d5f715bdfeff2984db3ca081853
Hugo Solleder <hsolleder@users.noreply.github.com>
2026-08-10 16:38:59 +0200

    feat(admin): update sign-in/-up page with modus design (#393)
    Closes #393

--- admin/src/pages/SigninPage.vue
+++ admin/src/pages/SigninPage.vue
@@ -1,13 +1,17 @@
 <template>
   <q-layout>
     <q-page-container>
-      <q-page class="flex flex-center bg-blue-grey-1">
+      <q-page class="flex flex-center auth-page">
+        <div class="background-container">
+          <img
+            src="/admin/PATTERN-VIOLET.svg"
+            aria-hidden="true"
+            class="background-pattern"
+          />
+        </div>
         <q-card :style="$q.screen.lt.sm ? { width: '80%' } : { width: '400px' }">
           <q-card-actions class="flex justify-center q-mt-xl q-ml-xl q-mr-xl q-mb-xs">
-            <img src="modus.svg" height="50px" />
-          </q-card-actions>
-          <q-card-actions class="flex justify-center">
-            <span class="text-primary text-h5 on-right">{{ t('main.brand') }}</span>
+            <img src="/admin/LOGO-VIOLET.svg" height="50px" />
           </q-card-actions>
           <q-card-actions class="flex justify-center q-mx-lg q-my-lg q-pa-none">
             <q-btn
@@ -58,3 +62,36 @@ async function onLogin() {
   }
 }
 </script>
+
+<style scoped>
+.q-card {
+  background-color: rgba(255, 255, 255, 0.95) !important;
+  backdrop-filter: blur(10px);
+}
+
+.background-container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+
+  pointer-events: none;
+  z-index: -1;
+}
+
+.background-pattern {
+  position: absolute;
+
+  top: -10rem;
+  left: 15rem;
+  width: 100rem;
+  height: 200rem;
+
+  object-fit: contain;
+  rotate: -40deg;
+
+  opacity: 0.05;
+}
+</style>
--- admin/src/pages/SignupPage.vue
+++ admin/src/pages/SignupPage.vue
@@ -1,13 +1,17 @@
 <template>
   <q-layout>
     <q-page-container>
-      <q-page class="flex flex-center bg-blue-grey-1">
+      <q-page class="flex flex-center auth-page">
+        <div class="background-container">
+          <img
+            src="/admin/PATTERN-VIOLET.svg"
+            aria-hidden="true"
+            class="background-pattern"
+          />
+        </div>
         <q-card :style="$q.screen.lt.sm ? { width: '80%' } : { width: '400px' }">
           <q-card-actions class="flex justify-center q-mt-lg q-ml-xl q-mr-xl q-mb-xs">
-            <img src="modus.svg" height="50px" />
-          </q-card-actions>
-          <q-card-actions class="flex justify-center">
-            <span class="text-primary text-h5 on-right">{{ t('main.brand') }}</span>
+            <img src="/admin/LOGO-VIOLET.svg" height="50px" />
           </q-card-actions>
           <q-card-section class="q-mx-lg q-my-md q-pa-none">
             <q-form ref="form">
@@ -196,3 +200,41 @@ function onCopyPassword() {
     .catch(notifyError)
 }
 </script>
+
+<style scoped>
+.q-card {
+  background-color: rgba(255, 255, 255, 0.95) !important;
+  backdrop-filter: blur(10px);
+}
+
+.auth-page {
+  position: relative;
+  overflow: hidden;
+}
+
+.background-container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+
+  pointer-events: none;
+  z-index: -1;
+}
+
+.background-pattern {
+  position: absolute;
+
+  top: -10rem;
+  left: 15rem;
+  width: 100rem;
+  height: 200rem;
+
+  object-fit: contain;
+  rotate: -40deg;
+
+  opacity: 0.05;
+}
+</style>
