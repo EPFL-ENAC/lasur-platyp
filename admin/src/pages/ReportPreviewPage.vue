bc497e7d338274ab45c34c7181440e00a3b13da1
ymarcon <yannick.marcon@epfl.ch>
2026-09-23 11:06:20 +0200

    polish: updated ROUE_DEM_MOBILITE-MOBILYSE in png #431

--- admin/src/pages/ReportPreviewPage.vue
+++ admin/src/pages/ReportPreviewPage.vue
@@ -246,8 +246,8 @@
 
         <q-markdown :src="t('generated_report.final_page_body')" />
 
-        <div class="text-center q-mt-lg">
-          <img src="/admin/V1-ROUE_DEM_MOBILITE-MOBILYSE.svg" alt="graph" />
+        <div class="text-center q-mt-xl">
+          <img src="/admin/ROUE_DEM_MOBILITE-MOBILYSE.png" alt="graph" style="max-width: 100%" />
         </div>
       </report-page>
     </div>
